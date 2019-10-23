14064512ff84f835bcbd50027525e3340fa94607
Arne Plöse <aploese@gmx.de>
2019-10-23 07:34:22 +0200

    fix docker script

--- docker-macosx.sh
+++ docker-macosx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mvn clean compile
 ./configure --host=x86_64-apple-darwin --with-jni-include-dir="$PWD/jni-includes/openjdk11 $PWD/jni-includes/openjdk11/darwin" \
 && make clean \
 && make 
