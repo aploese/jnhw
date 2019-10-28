081325fa0c42b5a0e2ad7429dbc5ed288049d6c1
Arne Ploese <aploese@gmx.de>
2019-10-28 12:26:53 +0100

    compiled FreeBSD

--- build-freebsd.sh
+++ build-freebsd.sh
@@ -1,1 +1,2 @@
-mvn clean compile && ./configure --with-jni-include-dir="/usr/local/openjdk8/include/ /usr/local/openjdk8/include/freebsd/" && gmake clean && gmake && mvn install
+export JAVA_HOME=/usr/local/openjdk11/
+mvn clean compile && ./configure --with-jni-include-dir="/usr/local/openjdk11/include/ /usr/local/openjdk11/include/freebsd/" && gmake clean && gmake && mvn install
