fa57cd152c50735739b32834b1e2a21f8274d161
Arne Pl;se <aploesegmx.de>
2020-07-29 18:18:29 +0200

    Changed test run

--- build-freebsd.sh
+++ build-freebsd.sh
@@ -1,2 +1,6 @@
 export JAVA_HOME=/usr/local/openjdk14/
-mvn clean compile  test-compile && ./configure --with-jni-include-dir="$JAVA_HOME/include/ $JAVA_HOME/include/freebsd/" && gmake clean && gmake && mvn install
+mvn clean compile  test-compile && \
+./configure --with-jni-include-dir="$JAVA_HOME/include/ $JAVA_HOME/include/freebsd/" && \
+gmake clean && \
+gmake && \
+mvn -fae install
