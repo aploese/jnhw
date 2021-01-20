491741d15d7f03075483626476aad92530257268
Arne Plöse <aploese@gmx.de>
2021-01-20 18:30:09 +0100

    fixes for Windows upper/lower case...

---
+++ de.ibapl.jnhw.posix/src/main/java/de/ibapl/jnhw/anontation/posix/poll/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * JNHW - Java Native header Wrapper, https://github.com/aploese/jnhw/
+ * Copyright (C) 2019-2020, Arne Plöse and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+/**
+ * Very thin and safe wrapper classes around common used headers on a POSIX system
+ * under the {@code sys} path.
+ *
+ * See specs at:
+ * <a href="https://pubs.opengroup.org/onlinepubs/9699919799/">The Open Group
+ * Base Specifications Issue 7, 2018 edition IEEE Std 1003.1-2017 (Revision of
+ * IEEE Std 1003.1-2008) Copyright © 2001-2018 IEEE and The Open Group</a>.
+ *
+ */
+package de.ibapl.jnhw.posix.sys;
