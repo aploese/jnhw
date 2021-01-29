ca3509b82fb4a1e9b38fdbf49927c2828be11aa2
Arne Plöse <aploese@gmx.de>
2021-01-29 14:34:56 +0100

    make examples it part of project, but dont install or deploy them

--- build-native.sh
+++ build-native.sh
@@ -8,6 +8,3 @@ mvn clean compile test-compile || exit 1
 make clean || exit 1
 make || exit 1
 mvn -fae install
-cd it
-mvn clean package
-cd ..
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mvn clean install
-cd it
-mvn clean package
-cd ..
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-mvn compile javadoc:jar source:jar deploy
+mvn clean compile javadoc:jar source:jar deploy
