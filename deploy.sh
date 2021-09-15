f7f0c9d17d1665b62b23e546593bd51f3d01c77b
Arne Plöse <aploese@gmx.de>
2021-09-15 08:47:13 +0200

    updates version and cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+mvn versions:display-plugin-updates -U -N
 mvn clean compile javadoc:jar source:jar deploy
--- docker-macosx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./configure --with-pic --host=x86_64-apple-darwin --with-jni-include-dir="$PWD/jni-includes/openjdk11 $PWD/jni-includes/openjdk11/darwin" \
-&& make clean \
-&& make 
